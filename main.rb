6b029ac65b7a05a58f33fb95edc70b5f4cc8011a
xiaodao <lychees67@gmail.com>
2016-05-25 21:48:16 +0800

    go go go
    ~

--- main.rb
+++ main.rb
@@ -6,42 +6,33 @@ Bundler.require
 
 configure {set :server, :puma}
 Faye::WebSocket.load_adapter('puma')
-
 sockets = Array.new
 
 get '/get' do
   ws = Faye::WebSocket.new request.env
-
   ws.on :open do |event|
     sockets << ws
   end
-
   ws.on :close do |event|
     sockets.delete ws
   end
-
   ws.rack_response
 end
 
 get '/send' do
   ws = Faye::WebSocket.new request.env
-
   ws.on :message do |event|
     sockets.each do |s|
       s.send(event.data)
     end
   end
-
   ws.rack_response
 end
 
-get '/hello/tfh' do
-  "Hello World"
+get '/tx/:tx' do |tx|
+  erb :tx, :locals => {:param1 => tx}
 end
 
-get '/tx/:id' do |id|
-  erb :tx, :locals => {:param1 => id}
-end
 
 
 get '/' do
